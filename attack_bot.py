b34c5ba7ceaa7e02875dddd6dbd394e8a494e9f6
gitforbot <87461070+gitforbot@users.noreply.github.com>
2021-09-08 11:24:14 +0900

    Add files via upload

---
+++ attack_bot.py
@@ -0,0 +1,43 @@
+from typing import AsyncContextManager
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    damage_add = 1
+    # '.damage' と入力を受け取ったとき
+    if message.content.startswith('.damage'):
+        tokkou = str(message.content)
+        tokkou_value = tokkou[7:]
+
+        # メッセージの中に '特攻' が含まれているとき
+        if 1 in tokkou_value:
+            damage_add *= 1.1
+
+        if 2 in tokkou_value:
+            damage_add *= 1.15
+        
+        if 3 in tokkou_value:
+            damage_add *= 1.23
+        
+        if 4 in tokkou_value:
+            damage_add *= 1.35
+        
+        if 4_5 in tokkou_value:
+            damage_add *= 1.4
+        
+        if 5 in tokkou_value:
+            damage_add *= 1.55
+    
+    print(damage_add)
+    await message.channel.send(f"{message.author.mention} 、{damage_add}")
+
+client.run('ODg0OTg2ODY2MjIxMzI2MzQ3.YTgePw.jvxLNGUcSseqwjKRcssHSM8SooY')
