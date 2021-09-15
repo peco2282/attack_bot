69b884fb84bbcef14221e5fa5be890d7ba9fb858
gitforbot <87461070+gitforbot@users.noreply.github.com>
2021-09-15 16:07:24 +0900

    Update attack_bot.py

--- attack_bot.py
+++ attack_bot.py
@@ -262,10 +262,11 @@ async def calc(message, dmg, os, tokkou):
                 tokkou_add *= 1.55
             
             if (str('leg') or str('LEG')) in tokkou:
-                tokkou_add *= 1.55
+                alpha= (dmg * 0.06)
+                print(alpha)
+                tokkou_add *= 1.55 * alpha
                 print(tokkou_add)
-                tokkou_add += (dmg * 0.06)
-                print(tokkou_add)
+                
                 #####
             alldmg = dmg * os_power * tokkou_add
             print(alldmg)
