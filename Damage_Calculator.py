15c79d59f68733af06c29a2106aeb08ec06aa71d
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-01-27 22:18:46 +0900

    Update Damage_Calculator.py

--- Damage_Calculator.py
+++ Damage_Calculator.py
@@ -12,7 +12,7 @@ from re_definition import tokkou, ctcalc, tokkoulist, embed_list
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='.', help_command=None, intents=intents)
 now = datetime.now(pytz.timezone('Asia/Tokyo'))
-token = 'ODg0OTg2ODY2MjIxMzI2MzQ3.YTgePw.jvxLNGUcSseqwjKRcssHSM8SooY'
+token = 'ODg0OTg2ODY2MjIxMzI2MzQ3.YTgePw.fNZi4vMTwhu_wkT3W7951RdgLoY'
 
 emoji_selector = u'\U0000fe0f\U000020e3'
 emoji_1 = u'\N{DIGIT ONE}' + emoji_selector
